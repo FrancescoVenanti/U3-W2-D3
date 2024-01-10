9ba064cc85ab5c76b7e1f71c08a8db03f0d8ccde
FrancescoVenanti <francesco.venanti@gmail.com>
2024-01-10 16:44:32 +0100

    fgdfg

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -3,34 +3,28 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const MovieDetails = () => {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route>
-					<Container>
-						<Row className=" justify-content-center">
-							<Col xs={6}>
-								<Card className="bg-black text-center">
-									<Card.Img
-										variant="top"
-										src={data.Poster}
-										style={{
-											width: "100%",
-											height: "600px",
-											objectFit: "contain",
-										}}
-									/>
-									<Card.Body>
-										<Card.Title className=" fw-bold fs-1">{data.Title}</Card.Title>
-										<Card.Text>{data.Plot}</Card.Text>
-										<Button variant="outline-light">Watch Now</Button>
-									</Card.Body>
-								</Card>
-							</Col>
-						</Row>
-					</Container>
-				</Route>
-			</Routes>
-		</BrowserRouter>
+		<Container>
+			<Row className=" justify-content-center">
+				<Col xs={6}>
+					<Card className="bg-black text-center">
+						<Card.Img
+							variant="top"
+							src={data.Poster}
+							style={{
+								width: "100%",
+								height: "600px",
+								objectFit: "contain",
+							}}
+						/>
+						<Card.Body>
+							<Card.Title className=" fw-bold fs-1">{data.Title}</Card.Title>
+							<Card.Text>{data.Plot}</Card.Text>
+							<Button variant="outline-light">Watch Now</Button>
+						</Card.Body>
+					</Card>
+				</Col>
+			</Row>
+		</Container>
 	);
 };
 export default MovieDetails;
